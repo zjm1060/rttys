7ea803459eb79a0ca89b5736be9ac90fb24f64a5
jianmin zhuang <zjm098@gmail.com>
2024-03-06 14:06:22 +0800

    heartbeat set 35 sec

--- device.go
+++ device.go
@@ -43,7 +43,7 @@ const (
 
 // Minimum protocol version requirements of rtty
 const rttyProtoRequired uint8 = 3
-const heartbeatInterval = time.Second * 5
+const heartbeatInterval = time.Second * 35
 
 type device struct {
 	br         *broker
